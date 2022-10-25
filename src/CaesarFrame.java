c750e398330d82282e6ac70cf48dcaa7241bfd11
Kaletris <papp.krisz9916@gmail.com>
2022-10-25 11:40:25 +0200

    Initial commit

--- src/CaesarFrame.java
+++ src/CaesarFrame.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class CaesarFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class CaesarFrame extends JFrame {
+    JTextField textField1 = new JTextField(20);
+    JTextField textField2 = new JTextField(20);
+    JButton button = new JButton("Code");
+    Object[] abc = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    JComboBox comboBox = new JComboBox(abc);
+
+    public CaesarFrame(){
+        setTitle("SwingLab");
+        setSize(400, 110);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        JLabel label = new JLabel("Output");
+        add(panel1, BorderLayout.NORTH);
+        add(panel2, BorderLayout.CENTER);
+        panel1.add(comboBox);
+        panel1.add(textField1);
+        panel1.add(button);
+        panel2.add(label);
+        panel2.add(textField2);
+        textField2.setEditable(false);
+
+        setResizable(true);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+        Main m = new Main();
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.hasNextLine()){
+            String[] line = scanner.nextLine().split(";");
+            String caesar = m.caesarCode(line[0], line[1].charAt(0));
+            System.out.println(caesar);
+        }
+         */
+        CaesarFrame caesarFrame = new CaesarFrame();
+        caesarFrame.setVisible(true);
+    }
+
+    private String caesarCode(String input, char offset){
+        char[] caesar = input.toUpperCase().toCharArray();
+        offset = (char)(Character.toUpperCase(offset) - 'A');
+        for(int i = 0; i < caesar.length; i++){
+            if(caesar[i] != ' ') {
+                if (caesar[i] + offset > 'Z') {
+                    caesar[i] = (char) (caesar[i] + offset + 'A' - 'Z' - 1);
+                } else {
+                    caesar[i] = (char) (caesar[i] + offset);
+                }
+            }
+        }
+
+        return new String(caesar);
     }
 }
